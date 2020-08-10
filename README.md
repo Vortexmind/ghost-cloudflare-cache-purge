c607eb9f837aa1806ab542f6707bed8e2fd5372a
Vortexmind <Vortexmind@users.noreply.github.com>
2020-08-10 21:14:51 +0100

    added prettier & npm run format

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
-# 👷‍♀️ `Cloudflare Cache Purger for Ghost Blog` 
+# 👷‍♀️ `Cloudflare Cache Purger for Ghost Blog`
 
 [![Total alerts](https://img.shields.io/lgtm/alerts/g/Vortexmind/ghost-cloudflare-cache-purge.svg?logo=lgtm&logoWidth=18)](https://lgtm.com/projects/g/Vortexmind/ghost-cloudflare-cache-purge/alerts/) [![Language grade: JavaScript](https://img.shields.io/lgtm/grade/javascript/g/Vortexmind/ghost-cloudflare-cache-purge.svg?logo=lgtm&logoWidth=18)](https://lgtm.com/projects/g/Vortexmind/ghost-cloudflare-cache-purge/context:javascript)
 
 This [Cloudflare Worker](https://workers.cloudflare.com/) can be used to execute a cache purge on a given Cloudflare zone when invoked. It uses HTTP Basic Authentication to allow only authorized users. This is the only authentication scheme currently supported by [Ghost Webhooks](https://ghost.org/docs/api/v3/webhooks/). This can be used to automate cache purging on certain actions on Ghost, for example new post publishing.
 
 What it does:
-- It accepts a request including an HTTP Basic Authentication header. 
+
+- It accepts a request including an HTTP Basic Authentication header.
 - If the credentials are invalid, an `HTTP 401` response is returned.
 - It decodes the header and compares it with pre-configured credentials.
 - If valid, it issues a [Cloudflare API](https://api.cloudflare.com/) call for a specific zone, and purges everything.
@@ -29,5 +30,4 @@ via wrangler.
 3. Edit the file, adding your Cloudflare Account Id, Zone ID and the `WEBHOOK_USER`, `CF_ZONE_ID` values
 4. Test your changes
 5. Use `wrangler secret put` to configure two secrets for your worker: `wrangler secret put CF_AUTH_TOKEN` with a Cloudflare API token with the Cache Purge permission for the intended zone. `wrangler secret put WEBHOOK_PASSWORD` with the password that will be associated to the above `WEBHOOK_USER`
-6. Once happy, run `wrangler publish` to deploy the worker 
-
+6. Once happy, run `wrangler publish` to deploy the worker
--- index.js
+++ index.js
@@ -2,74 +2,68 @@ import auth from 'basic-auth'
 import compare from 'tsscmp'
 
 function unauthorizedResponse(body) {
-  return new Response(
-    body, {
-      status: 401,
-      headers: {
-        "WWW-Authenticate": 'Basic realm="User Visible Realm"'
-      }
-    }
-  )
-}
-
-function generateSimpleResponse(httpCode, strMessage){
-  return new Response (
-    JSON.stringify({"result": strMessage}), {
-      status: httpCode,
-      headers : { 
-        'Content-Type' : 'application/json'
-      }
+  return new Response(body, {
+    status: 401,
+    headers: {
+      'WWW-Authenticate': 'Basic realm="User Visible Realm"',
+    },
   })
 }
 
-function check(name,pass) {
+function generateSimpleResponse(httpCode, strMessage) {
+  return new Response(JSON.stringify({ result: strMessage }), {
+    status: httpCode,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+}
+
+function check(name, pass) {
   var valid = true
- 
+
   valid = compare(name, WEBHOOK_USER) && valid
   valid = compare(pass, WEBHOOK_PASSWORD) && valid
 
   return valid
 }
 
-async function purgeCache() { 
-  const apiUrl = "https://api.cloudflare.com/client/v4/zones/"+CF_ZONE_ID+"/purge_cache"
+async function purgeCache() {
+  const apiUrl =
+    'https://api.cloudflare.com/client/v4/zones/' + CF_ZONE_ID + '/purge_cache'
 
   const apiPayload = {
-     "purge_everything":true
+    purge_everything: true,
   }
 
   const init = {
     body: JSON.stringify(apiPayload),
     method: 'POST',
-    headers : {
-      'Authorization' : "Bearer " + CF_AUTH_TOKEN
-    }
+    headers: {
+      Authorization: 'Bearer ' + CF_AUTH_TOKEN,
+    },
   }
 
   const response = await fetch(apiUrl, init)
   return await response.json()
-
 }
 
-addEventListener('fetch', event => {
+addEventListener('fetch', (event) => {
   event.respondWith(handleRequest(event.request))
 })
 
 async function handleRequest(request) {
+  var credentials = auth.parse(request.headers.get('Authorization'))
 
-  var credentials = auth.parse(request.headers.get("Authorization"))
-
-  if ( !credentials || !check(credentials.name, credentials.pass) ) {
-    return unauthorizedResponse("Unauthorized")
+  if (!credentials || !check(credentials.name, credentials.pass)) {
+    return unauthorizedResponse('Unauthorized')
   } else {
     const responseData = await purgeCache()
 
-    if (responseData.success){ 
-      return generateSimpleResponse (200,"OK")
-    } else  {
-       return generateSimpleResponse (500,"FAILED")
+    if (responseData.success) {
+      return generateSimpleResponse(200, 'OK')
+    } else {
+      return generateSimpleResponse(500, 'FAILED')
     }
-
   }
 }
-
